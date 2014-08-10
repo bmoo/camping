363388769b2f62a5be05f8911abaa13366ea9478
Brad Moore <brad@bradmoore.org>
2014-08-10 13:45:56 -0700

    Test iOS app

---
+++ ios/Camping/Camping/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Camping
+//
+//  Created by Brad Moore on 8/10/14.
+//  Copyright (c) 2014 Brad Moore. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
